376ab525827828d660d59c77624571fd4c03b827
Manuel López <thedarkymagic@gmail.com>
2020-03-25 17:40:58 -0300

    Changed JSON structure

--- lib/src/models/foundation.dart
+++ lib/src/models/foundation.dart
@@ -2,26 +2,23 @@ import 'package:flutter/foundation.dart';
 
 class Foundation {
   const Foundation({
-    this.id,
-    this.imageUrl,
-    this.name,
-    this.balance,
-    this.goal,
+    @required this.id,
+    @required this.name,
+    @required this.balance,
+    @required this.goal,
   });
 
   final int id;
-  final String imageUrl;
   final String name;
   final int balance;
   final int goal;
 
   factory Foundation.fromJson(Map<String, dynamic> json) {
     return Foundation(
-      id: json['id_fundacion'],
-      imageUrl: json['foto'],
-      name: json['nombre_fundacion'],
-      balance: json['dinero_total'],
-      goal: json['monto'],
+      id: json['id'],
+      name: json['name'],
+      balance: json['balance'],
+      goal: json['goal'],
     );
   }
 }
