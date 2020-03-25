4fed39da8e1fdfd09817f1a06bd4828975cee0f6
Manuel López <thedarkymagic@gmail.com>
2020-03-25 17:40:20 -0300

    Removed unused resources

--- lib/src/services/auth/auth_service.dart
+++
@@ -1,7 +0,0 @@
-import 'package:covid_19/src/models/user.dart';
-
-abstract class AuthService {
-  Future<User> signInWithEmailAndPassword(String email, String password);
-  Future<void> signOut();
-  void dispose();
-}
